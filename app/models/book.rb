974f7de6475ba343a157029f3417eb7417b10bb2
suzu784 <s.suzuki784@hotmail.com>
2022-06-05 10:12:48 +0000

    [Finish]9c

---
+++ app/models/book.rb
@@ -0,0 +1,23 @@
+class Book < ApplicationRecord
+
+  has_one_attached :image
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
+
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+end
